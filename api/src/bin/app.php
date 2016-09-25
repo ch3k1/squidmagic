481c2eaaee06a0110162f18962c1bc1c9416ea63
Aleksandre Vardanidze <avardanidze@gmail.com>
2016-09-25 16:25:52 +0400

    Update app.php

--- api/src/bin/app.php
+++ api/src/bin/app.php
@@ -4,6 +4,11 @@ namespace SquidApp;
 require dirname(__DIR__) . '/../vendor/autoload.php';
 $pusher = new Ipaddr();
 
-$pusher->setAddress('192.168.0.111');
-print_r($pusher->getAddress() . PHP_EOL);
+if ($argc !== 2) {
+    echo "Usage: php app.php [ipaddress].\n";
+    exit(1);
+}
+$ip = $argv[1];
 
+$pusher->setAddress($ip);
+print_r($pusher->getAddress() . PHP_EOL);
