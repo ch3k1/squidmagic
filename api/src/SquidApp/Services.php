aa9c9fedd9b53a1ad494653bfde87f3c510fe54a
Aleksandre Vardanidze <avardanidze@gmail.com>
2016-09-27 23:31:40 +0400

    Update Services.php

--- api/src/SquidApp/Services.php
+++ api/src/SquidApp/Services.php
@@ -17,15 +17,15 @@ class Services
 
 	public function detectService($service) {
 
-		  $services = array(80 => 'http', 21 => 'ftp', 22 => 'ssh');
+	  $services = array(80 => 'http', 21 => 'ftp', 22 => 'ssh');
           $port = array_search($service, $services);
 
           if($port !== NULL) {
-          	  $this->service = $port;
+             $this->service = $port;
           }
 	}
 
 	public function getServiceName() {
-		  return $this->service;
+             return $this->service;
 	}
 }
