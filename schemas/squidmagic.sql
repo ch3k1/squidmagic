1f4a7e75c31f0131883dcc997fb9f8333bff6290
Aleksandre Vardanidze <avardanidze@gmail.com>
2016-12-04 15:12:53 +0400

    CREATE TABLE IF NOT EXISTS  (

---
+++ schemas/squidmagic.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `squidmagic_c2c` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ipaddress` varchar(20) NOT NULL,
+  `squid` varchar(16) NOT NULL,
+  `status` varchar(16) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
