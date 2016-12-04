3adbc3017d1f012e97067a408bd08c2ddfbdf8cd
Aleksandre Vardanidze <avardanidze@gmail.com>
2016-12-04 15:40:37 +0400

    squidmagic db

--- schemas/squidmagic.sql
+++ schemas/squidmagic.sql
@@ -1,3 +1,21 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `squidmagic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `squidmagic_c2c`
+--
 
 CREATE TABLE IF NOT EXISTS `squidmagic_c2c` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
