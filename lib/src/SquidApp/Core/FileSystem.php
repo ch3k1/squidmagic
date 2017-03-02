21065430b48771e5be706c46785a5ff7762ddcc3
ch3k1 <avardanidze@gmail.com>
2017-03-02 11:28:09 +0400

    fixed Exception

--- lib/src/SquidApp/Core/FileSystem.php
+++ lib/src/SquidApp/Core/FileSystem.php
@@ -13,21 +13,31 @@ class FileSystem
 	public static function scandirs($path) {
 
 		// Scans a directory for files
-	    $scandir = scandir($path);
-	    if($scandir === false) {
-            throw new \SquidApp\Exception\ServerFolderNotFoundException('Server Folder Not found');
-	    }
-
+	    try { 
+	    	$scandir = scandir($path);
+		    if($scandir === false) {
+	            throw new \SquidApp\Exception\ServerFolderNotFoundException('Server Folder Not found');
+		    }
+		}
+		catch (\SquidApp\Exception\ServerFolderNotFoundException $e) {
+                throw new \SquidApp\Exception\ServerFolderNotFoundException($e->getMessage());
+        }
+	    
 	}
 
 	public static function fileExists($path) {
 
 		// Checks if file exists in certain location 
-		$fileExists = file_exists($path);
-	    if($fileExists === false) {
-            throw new \SquidApp\Exception\FileExistsException('Collector File Not found');
-	    }
-
+		try { 
+			$fileExists = file_exists($path);
+		    if($fileExists === false) {
+	            throw new \SquidApp\Exception\FileExistsException('Collector File Not found');
+		    }
+		}
+		catch (\SquidApp\Exception\FileExistsException $e) {
+                throw new \SquidApp\Exception\FileExistsException($e->getMessage());
+        }
+	    
 	}
 
 	public static function openInBackground($path) {
